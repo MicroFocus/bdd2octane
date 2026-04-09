67334fed7eeebc43914703c9963e76e7ef013873
Micheleh <46257396+micheleh@users.noreply.github.com>
2026-04-09 14:40:53 +0300

    Fix XXE vulnerability in JunitReportReader (#49)
    Disable external entity resolution and DTD processing on the
    XMLInputFactory used to parse JUnit XML reports. Without this,
    a crafted report file could read arbitrary local files or cause
    denial of service via entity expansion.

--- src/main/java/com/microfocus/bdd/JunitReportReader.java
+++ src/main/java/com/microfocus/bdd/JunitReportReader.java
@@ -53,6 +53,8 @@ public class JunitReportReader implements Iterable<Element>, Closeable {
     public JunitReportReader(InputStream inputStream, String testcaseElementName) throws XMLStreamException {
         this.inputStream = inputStream;
         XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        xmlInputFactory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+        xmlInputFactory.setProperty(XMLInputFactory.SUPPORT_DTD, false);
         reader = xmlInputFactory.createXMLEventReader(inputStream, StandardCharsets.UTF_8.name());
         this.testcaseElementName = testcaseElementName;
         iterator = new ElementIterator();
